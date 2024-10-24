330d800fcd80bdf39cb37af14f2311156bbedf7b
docar <weused@gmail.com>
2024-10-24 18:40:30 +0500

    Implemented Lab Exam Tasks

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+  const menuItems = document.getElementById("menuItems");
+  menuItems.style.display =
+    menuItems.style.display === "flex" ? "none" : "flex";
+}
+
+function scrollToSection(event) {
+  event.preventDefault(); // Prevent the default anchor click behavior
+  const targetId = event.target.getAttribute("href").substring(1); // Get the ID of the target section
+  const targetElement = document.getElementById(targetId); // Find the target element
+
+  if (targetElement) {
+    // Scroll to the target element
+    targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+    toggleMenu(); // Close the menu after selecting an item
+  }
+}
