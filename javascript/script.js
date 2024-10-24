ff9b44b9991e94eb0f3049d409fc0c3732ab9e6c
docar <weused@gmail.com>
2024-10-24 19:06:57 +0500

    This Is My Final Exam For Mid Lab Exam

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,11 @@
+// Toggle Function
 function toggleMenu() {
   const menuItems = document.getElementById("menuItems");
   menuItems.style.display =
     menuItems.style.display === "flex" ? "none" : "flex";
 }
 
+// Scroll Function
 function scrollToSection(event) {
   event.preventDefault(); // Prevent the default anchor click behavior
   const targetId = event.target.getAttribute("href").substring(1); // Get the ID of the target section
